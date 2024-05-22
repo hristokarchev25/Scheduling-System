faa1f76788c5366af199a4b68c41d329c31910e6
Hristo Karchev <x_karchev@mail.bg>
2024-05-22 14:25:47 +0300

    add/ home page connected to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './azzurroLogo.jpg';
-import './App.css';
+/* import logo from './azzurroLogo.jpg';
+import './App.css'; */
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/about" exact component={About} />
+        <Route path="/questions" exact component={Questions} />
+        <Route component={Error} /> */}
+      </Routes>
+      {/*  <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+      </header> */}
+    </>
   );
 }
 
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './HomePage.css';
+import logo from '../../utils/azzurroLogo.jpg';
 
-function HomePage() {
+const HomePage = function HomePage() {
     return (
         <div className="Home-page">
             <header className="Home-header">
+                <p>loreamfmf</p>
                 <img src={logo} className="App-logo" alt="logo" />
             </header>
         </div>
