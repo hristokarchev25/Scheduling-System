da65ae7b32efbf666c0cf85b258e039cc0c589c3
Hristo Karchev <x_karchev@mail.bg>
2024-05-22 14:04:36 +0300

    add/home page created + home css

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+    return (
+        <div className="Home-page">
+            <header className="Home-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    )
+}
+
+export default HomePage;
