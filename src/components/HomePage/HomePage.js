823096040adde7380c9ec04d781b5a19880f7c4f
Hristo Karchev <x_karchev@mail.bg>
2024-06-03 15:22:16 +0300

    add/ video background added to the start page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css';
 import logo from '../../utils/azzurroLogo.jpg';
+import videoBg from '../../utils/homeVideo.mp4';
 
 const HomePage = function HomePage() {
     return (
-        <div className="Home-page">
-            <header className="Home-header">
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>
-            <div>
+        <div className='main'>
+            <div className="overlay"></div>
+            <video src={videoBg} autoPlay loop muted />
+            <div className="content">
+                <header className="Home-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+                <h1>Welcome</h1>
+                <p className='welcome-text'>To the scheduling system of Azzurro Beach Bar.</p>
                 <Link to="/scheduling">
-                    <button className="quiz__btn">Start</button>
+                    <button className="button-89 ">Continue</button>
                 </Link>
             </div>
         </div>
